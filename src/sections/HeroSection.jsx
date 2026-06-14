c0b057d5e699e3b1ca4c5c5d07efda1286ca5e45
Nikola Tomic <tomicnikolaftn@gmail.com>
2026-06-14 03:07:32 +0200

    profile font update

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -5,8 +5,12 @@ export default function HeroSection({ id }) {
         <p className="hero-names">Martin &amp; Alena</p>
 
         <div className="hero-love-row">
-          <p className="hero-vertical-text">sacuvajte dan za nas</p>
-          <img className="hero-love-image" src="/love-profile-cropped.png" alt="Love" />
+          <p className="hero-vertical-text">Sačuvajte dan za nas</p>
+          <img
+            className="hero-love-image"
+            src="/love-profile-cropped.png"
+            alt="Love"
+          />
         </div>
       </div>
     </section>
